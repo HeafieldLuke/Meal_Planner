e041a578d3d11228c82292806000225cd143b6f6
Luke Heafield <lukeheafield@MacBook-Pro.local>
2020-07-14 23:20:00 -0400

    Added database, wip branch

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const PORT = 3000;
+
+mongoose.connect('mongodb://localhost:27017/testdb', {
+    useNewUrlParser: 'true'
+})
+
+mongoose.connection.on('error', err => {
+    console.log('err', err);
+})
+
+mongoose.connection.on('connected', (err,res) => {
+    console.log('mongoose is connected');
+})
 
 app.set('views', __dirname + '/public/views')
 app.set("view engine", 'ejs');
